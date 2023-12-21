c185a7d0b965a9d1f241d2f3e4c82f318364c27e
petromalskyi <113782385+petromalskyi@users.noreply.github.com>
2023-12-21 13:47:47 +0000

    Deploying to gh-pages from @ petromalskyi/goit-js-hw-10@71c4aa7a38120a4a77a47029e33a422999cf1d9c 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";import"./assets/vendor-f0698ccd.js";const l=document.querySelector("form");l.addEventListener("submit",a);function a(t){t.preventDefault();const e=t.target.elements,o=e.delay.value;console.log(o);const n=e.state.value;console.log(n)}
+//# sourceMappingURL=commonHelpers2.js.map
