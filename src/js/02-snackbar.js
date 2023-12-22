f3dba02f15ff5fa2db7d7bc5dff35f6881d8a9ec
petromalskyi <113782385+petromalskyi@users.noreply.github.com>
2023-12-22 11:44:39 +0200

    finished  hw-10

--- src/js/02-snackbar.js
+++ src/js/02-snackbar.js
@@ -1,9 +1,6 @@
 import 'izitoast/dist/css/iziToast.min.css';
 import iziToast from 'izitoast/dist/js/iziToast.min.js';
 
-// const inputEl = document.querySelector('.form-delay-input');
-// console.log(inputEl);
-
 const formEl = document.querySelector('form');
 
 formEl.addEventListener('submit', handleSubmit);
@@ -14,32 +11,6 @@ function handleSubmit(event) {
   const delay = form.delay.value;
   const state = form.state.value;
   formEl.reset();
-
-  //   setTimeout(() => {
-  // if (state === 'fulfilled') {
-  //   //   console.log(`✅ Fulfilled promise in ${delay}ms`);
-  //   iziToast.success({
-  //     title: 'OK',
-  //     message: `✅ Fulfilled promise in ${delay}ms`,
-  //     position: 'topRight',
-  //     messageColor: '#ffffff',
-  //     titleColor: '#ffffff',
-  //     iconColor: '#ffffff',
-  //     backgroundColor: '#59A10D',
-  //   });
-  // } else {
-  //   //   console.error(`❌ Rejected promise in ${delay}ms`);
-  //   iziToast.error({
-  //     title: 'Error',
-  //     message: `❌ Rejected promise in ${delay}ms`,
-  //     position: 'topRight',
-  //     messageColor: '#ffffff',
-  //     titleColor: '#ffffff',
-  //     iconColor: '#ffffff',
-  //     backgroundColor: '#B51B1B',
-  //   });
-  // }
-  //   }, delay);
 
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -73,6 +44,5 @@ function handleSubmit(event) {
         iconColor: '#ffffff',
         backgroundColor: '#B51B1B',
       });
-    })
-    .finally(() => console.log('Promise settled')); // "Promise settled"
+    });
 }
